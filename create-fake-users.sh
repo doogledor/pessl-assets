e60b5b37e34e30c760cddab00971273e98084dff
Sam Elie <samradelie@gmail.com>
2020-07-29 19:25:23 -0700

    users

--- create-fake-users.sh
+++ create-fake-users.sh
@@ -4,6 +4,10 @@ rm users/users.json
 echo "[" >> users/users.json
 for i in {1..100}; do
     content=$(curl -H "Content-Type: application/json" -s "http://faker.hook.io?property=helpers.userCard&local=de,us,fr")
+    if [ $i -gt 1 ]
+    then
+      echo , >>  users/users.json
+    fi
     echo $content
     echo "$content" >>  users/users.json
 done
